9e10ea096f44905a9ead81e4af8c776f6d3713e7
Adam Lee <adamhlee@hotmail.ca>
2020-02-20 20:06:52 -0500

    Add Empty Project

---
+++ 2D Platformer/Assets/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    private float movementInputDirection;
+
+    Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    public float jumpForce;
+
+    public Transform groundCheck;
+
+    private bool isGrounded;
+
+    public float groundCheckRadius;
+
+    public LayerMask whatIsGround;
+
+    private bool canJump;
+
+    public float jumpSpeed;
+      
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkInput();
+        CheckIfCanJump();
+    }
+
+    private void FixedUpdate()
+    {
+        applyMovement();
+        CheckSurroundings();
+    }
+
+    private void checkInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKey("w") || Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void Jump()
+    {
+        if(canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce*jumpSpeed);
+        }
+       
+        
+    }
+
+    private void applyMovement()
+    {
+        rb.velocity = new Vector2(moveSpeed * movementInputDirection, rb.velocity.y);
+    }
+
+    private void CheckSurroundings()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+}
